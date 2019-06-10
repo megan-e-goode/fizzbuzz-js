f3da49536fd1a4691fb20fea2510c07768b06815
Megan Goode <megan.e.goode@hotmail.com>
2019-06-10 13:57:50 +0100

    First commit

---
+++ jasmine/spec/FizzbuzzSpec.js
@@ -0,0 +1,11 @@
+describe("title of test suite", function() {
+  // comment
+
+  it("feature of test suite", function () {
+    expect(1 + 2).toBe(3);
+  });
+
+  it("feature two of test suite", function () {
+    pending();
+  });
+});
